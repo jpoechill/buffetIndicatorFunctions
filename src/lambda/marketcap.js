82a565f7db2a79ee2e62967aa91f632db3dc42d7
Po Rith <poamrong@rith.com>
2020-08-14 14:49:40 -0700

    add marketcap

---
+++ src/lambda/marketcap.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+// const cors = require('cors')
+
+let articles = new Set()
+
+const fetchData = async () => {
+    const siteUrl = "https://designboom.com/";
+    const result = await axios.get(siteUrl);
+
+    return cheerio.load(result.data);
+};
+
+const getResults = async () => {
+    const $ = await fetchData();
+
+    // clear the cache, or you will see data from prev call!
+    // articles.clear(); 
+    
+    // $("article[class=dboom-article-most-recent]").each((index, element) => {
+    //     let title = $('.dboom-title', element).text().substr(1)
+    //     let date = $('.dboom-image .date-info', element).text()
+    //     let imgSrc = $('.dboom-image a img', element).data().lazySrc
+    //     let description = $('.dboom-excerpt', element).first().text()
+    //     let tags = $('.dboom-tags a', element).map((index, el) => {
+    //         return $(el).text()
+    //     }).get()
+
+    //     articles.add({
+    //         title: title,
+    //         date: date,
+    //         urlToImage: imgSrc,
+    //         description: description,
+    //         tags: tags
+    //     });
+    // });
+
+    return
+};
+
+exports.handler = (event, context, callback) => {
+    getResults()
+        .then((res) => {
+            callback(null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'application/json',
+                    'Access-Control-Allow-Methods': 'GET',
+                    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                },
+                body: JSON.stringify([
+                    {
+                        'bird': 'word'
+                    }
+                ]),
+            });
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
